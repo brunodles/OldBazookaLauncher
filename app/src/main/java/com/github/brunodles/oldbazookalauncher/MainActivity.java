9793ccb46e955dd821bdd6133e3441b70906f29b
Bruno de Lima <dlimaun@gmail.com>
2016-08-23 07:33:33 -0300

    Initial commit

---
+++ app/src/main/java/com/github/brunodles/oldbazookalauncher/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.brunodles.oldbazookalauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by bruno on 22/08/16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
